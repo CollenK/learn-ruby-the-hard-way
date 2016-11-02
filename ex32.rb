1d2a6296606507e59eaa6b0bcf07544f9ed1c250
CollenK <collen.kriel@gmail.com>
2016-11-02 15:59:22 +0700

    Complete

--- ex32.rb
+++ ex32.rb
@@ -27,7 +27,7 @@ elements = []
 (0..5).each do |i|
   puts "adding #{i} to the list."
   # pushes the i variable on the *end* of the list
-  elements.push(i)
+  elements << i # use shovel instead of .push(i)
 end
 
 # now we can print them out too
